99c5eaa7bbea0df0fe52a36f6045f5588a9fe12a
samarasrs <smr.samara.srs@gmail.com>
2020-06-09 01:03:05 -0300

    atualização

--- 04_Lista/q03_lista4.py
+++ 04_Lista/q03_lista4.py
@@ -1,5 +1,68 @@
 from __future__ import generators
-from . import q01_lista4
+from abc import ABC, abstractmethod
+
+class Veiculos():
+    @abstractmethod
+    def factory(tipo, motor):
+        if tipo == 'automovel':
+            return Automovel(motor)
+        elif tipo == 'caminhao':
+            return Caminhao(motor)
+        else:
+            raise NameError(" Tipo de veiculo  invalido. Veiculos disponiveis: automovel e caminhao")
+
+    def set_motor(self, motor):
+        if motor == 'eletrico':
+            Motor_eletrico()
+
+        elif motor == 'combustao':
+            Motor_combustao()
+
+        elif motor == 'hibrido':
+            Motor_hibrido()
+
+        else:
+            raise NameError(" Tipo do motor invalido. Motores disponiveis: eletrico, combustao ou hibrido")
+
+class Automovel(Veiculos):
+    def __init__(self, motor):
+        self.set_motor(motor)
+        self.motor = motor
+
+    def descricao(self):
+        print("Automovel como motor", self.motor)
+
+
+class Caminhao(Veiculos):
+    def __init__(self, motor):
+        self.set_motor(motor)
+        self.motor = motor
+
+    def descricao(self):
+        print("Caminhao como motor", self.motor)
+
+class Motor():
+    def descricao(self):
+        pass
+
+
+class Motor_eletrico(Motor):
+    def descricao(self):
+        print("O motor é elétrico")
 
 
 
+class Motor_combustao(Motor):
+    def descricao(self):
+        print("O motor é a combustão")
+
+
+class Motor_hibrido(Motor):
+    def descricao(self):
+        print("O motor é híbrido")
+
+
+
+
+car = Veiculos.factory('automovel', 'eletrico')
+car.descricao()
